62f41afb49e9ead1bc5c79208304acc5977243d0
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-22 14:14:18 +0530

    [Ariprasath] added: Create multiple contacts. OOPS concepts used

---
+++ Contacts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUsingOOPS
+{
+    class Contacts
+    {
+        string firstName, lastName, address, city, state, zipCode, phoneNumber, eMail;
+        public Contacts(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+            this.address = Address;
+            this.city = City;
+            this.state = State;
+            this.zipCode = ZipCode;
+            this.phoneNumber = PhoneNumber;
+            this.eMail = Email;
+        }
+        public void toString()
+        {
+            Console.WriteLine(this.firstName + " " + this.lastName + "\n" + this.address + "\n" + this.city + "\n" + this.state + "\n" + this.zipCode + "\n" + this.phoneNumber + "\n" + this.eMail);
+        }
+    }
+}
---
+++ MainMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookUsingOOPS
+{
+    class MainMethod
+    {
+        public static void Main(String[] args)
+        {
+            Contacts person1 = new Contacts("Ariprasath", "Sakthivel", "No.1, A street, b cross", "XYZ city", "MNO state", "606606", "9494946060", "ariprasath3@gmail.com");
+            Contacts person2 = new Contacts("Rahul", "Krishna", "No.2,AK street, 3rd Cross", "XXY city", "MMN state", "660666", "9194979295", "rahulkrisna@gmail.com");
+            person1.toString();
+            person2.toString();
+        }
+    }
+}

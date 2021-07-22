870e6e100e51fbb2c2ea682e1f6167022a197720
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-22 13:35:22 +0530

    [Ariprasath] added: create a contact that has name, address, city, state, zip code, Phone numeber, email

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace AddressBookUsingOOPS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Contacts("Ariprasath", "Sakthivel", "No.1, A street, b cross", "XYZ city", "MNO state", "606606", "9494946060", "ariprasath3@gmail.com");
+        }
+        public static void Contacts(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            string firstName = FirstName, lastName = LastName, address = Address, city = City, state = State, zipCode = ZipCode, phoneNumber = PhoneNumber, eMail = Email;
+            Console.WriteLine(firstName + " " + lastName + "\n" + address + "\n" + city + "\n" + state + "\n" + zipCode + "\n" + PhoneNumber + "\n" + Email);
         }
     }
 }
